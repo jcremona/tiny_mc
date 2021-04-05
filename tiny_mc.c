2298a1605f046f2cca4fedaa39710708b764d208
Javier Cremona <cremona@cifasis-conicet.gov.ar>
2021-04-05 12:16:09 -0300

    Save results in two files instead of printing them

--- tiny_mc.c
+++ tiny_mc.c
@@ -80,8 +80,22 @@ static void photon(void)
  * Main matter
  ***/
 
-int main(void)
+int main(int argc, char* argv[])
 {
+    char* heat_filepath;
+    char* photons_per_sec_filepath;
+    if (argc == 3) {
+        heat_filepath = argv[1];
+        photons_per_sec_filepath = argv[2];
+    } else {
+        if (argc == 2) {
+            fprintf(stderr, "ERROR: Argument missed!\nUsage: %s <HEAT_FILE_PATH> <PHOTONS_PER_SEC_FILE_PATH>\n", argv[0]);
+            return -1;
+        }
+        heat_filepath = "heat.txt";
+        photons_per_sec_filepath = "photons_per_sec.txt";
+    }
+
     // heading
     printf("# %s\n# %s\n# %s\n", t1, t2, t3);
     printf("# Scattering = %8.3f/cm\n", MU_S);
@@ -101,16 +115,21 @@ int main(void)
     assert(start <= end);
     double elapsed = end - start;
 
+    FILE* heat_fp = fopen(heat_filepath, "w");
+    FILE* photons_fp = fopen(photons_per_sec_filepath, "a");
+    printf("# Heat filepath: %s \n", heat_filepath);
+    printf("# Photons filepath: %s \n", photons_per_sec_filepath);
     printf("# %lf seconds\n", elapsed);
     printf("# %lf K photons per second\n", 1e-3 * PHOTONS / elapsed);
+    fprintf(photons_fp, "%lf\n", PHOTONS / elapsed);
 
     printf("# Radius\tHeat\n");
     printf("# [microns]\t[W/cm^3]\tError\n");
     float t = 4.0f * M_PI * powf(MICRONS_PER_SHELL, 3.0f) * PHOTONS / 1e12;
     for (unsigned int i = 0; i < SHELLS - 1; ++i) {
-        printf("%6.0f\t%12.5f\t%12.5f\n", i * (float)MICRONS_PER_SHELL,
-               heat[i] / t / (i * i + i + 1.0 / 3.0),
-               sqrt(heat2[i] - heat[i] * heat[i] / PHOTONS) / t / (i * i + i + 1.0f / 3.0f));
+        fprintf(heat_fp, "%6.0f\t%12.5f\t%12.5f\n", i * (float)MICRONS_PER_SHELL,
+                heat[i] / t / (i * i + i + 1.0 / 3.0),
+                sqrt(heat2[i] - heat[i] * heat[i] / PHOTONS) / t / (i * i + i + 1.0f / 3.0f));
     }
     printf("# extra\t%12.5f\n", heat[SHELLS - 1] / PHOTONS);
 
