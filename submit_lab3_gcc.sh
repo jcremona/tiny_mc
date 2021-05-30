a4e1369d97f12ba1fd16b08057b35710cc13e7d7
Javier Cremona <cp202143@jupiterace.famaf.unc.edu.ar>
2021-05-30 17:00:37 -0300

    Slurm scripts

---
+++ submit_lab3_gcc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --job-name=tinymc
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --exclusive
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+NUM_ITER=25
+
+if [ $SLURM_CPUS_PER_TASK -gt 1 ] ; then
+	srun ./execute_n_times.sh -p -n $NUM_ITER gcc
+else
+	srun ./execute_n_times.sh -n $NUM_ITER gcc
+fi
+
+# Run using -c (cpus per task)
+# sbatch -c <NUM_OF_THREADS> submit_lab3_gcc.sh
+
